5dc790b975c8ade77de06ce960e21d6fa2e2165a
Xochitl Castrejon-Stukes <90938040+castrejonx@users.noreply.github.com>
2024-07-30 20:06:05 +0000

    Added QR Info to website content

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const petNameElement = document.querySelector('#pet-name span');
     const ownerPhoneElement = document.querySelector('#owner-phone span');
+    const qrInfoElement = document.querySelector('#owner-phone span');
     const vetInfoElement = document.querySelector('#vet-info span');
 
     // Sneaky data
     const petData = {
         name: 'Sneaky',
         ownerPhone: '+2539730573',
+        qrInfo: "Sneaky''s owners have already been alerted that her QR code has been scanned.",
         vetInfo: 'Arroyo Veterinary Clinic, (575) 524-0900'
     };
 
     // Populate the pet info
     petNameElement.textContent = petData.name;
     ownerPhoneElement.textContent = petData.ownerPhone;
+    qrInfoElement.textContent = petData.qrInfo;
     vetInfoElement.textContent = petData.vetInfo;
 });
