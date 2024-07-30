9482893a74e57afa62faebe95c300b93f79883dd
Xochitl Castrejon-Stukes <90938040+castrejonx@users.noreply.github.com>
2024-07-30 20:25:53 +0000

    Rearranged QR info content, 2nd try

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const petNameElement = document.querySelector('#pet-name span');
+    const qrInfoElement = document.querySelector('#qr-info span');
     const ownerPhoneElement = document.querySelector('#owner-phone span');
     const vetInfoElement = document.querySelector('#vet-info span');
 
@@ -7,11 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const petData = {
         name: 'Sneaky',
         ownerPhone: '+2539730573',
+        qrInfo: "",
         vetInfo: 'Arroyo Veterinary Clinic (575) 524-0900'
     };
 
     // Populate the pet info
     petNameElement.textContent = petData.name;
+    qrInfoElement.textContent = petData.qrInfo;
     ownerPhoneElement.textContent = petData.ownerPhone;
     vetInfoElement.textContent = petData.vetInfo;
 });
